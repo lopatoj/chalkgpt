42d550f5f6ca27a82b4ddab764b73e659b04e710
lopatoj <justin@lopato.org>
2023-04-19 10:53:29 -0500

    hmmm

--- pages/api/openai/config.js
+++ pages/api/openai/config.js
@@ -2,6 +2,7 @@ const { Configuration, OpenAIApi } = require("openai");
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
+  organization: "org-I4tlqMCIWp6VH2uqe63Owqwi",
 });
 
 delete configuration.baseOptions.headers['User-Agent'];
