0fec022e167e588947af8d8e4d0dd0999f5e50bd
lopatoj <justin@lopato.org>
2023-04-19 05:37:46 -0500

    dgffd

--- pages/api/openai/config.js
+++ pages/api/openai/config.js
@@ -5,6 +5,4 @@ const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
 
-delete configuration.baseOptions.headers['User-Agent'];
-
 export const openai = new OpenAIApi(configuration);
