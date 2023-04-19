2b467eb6aef435f456708863fdb45c1f9a8b0ad3
lopatoj <justin@lopato.org>
2023-04-19 03:37:43 -0500

    push

--- pages/api/openai/config.js
+++ pages/api/openai/config.js
@@ -1,7 +1,7 @@
 const { Configuration, OpenAIApi } = require("openai");
 
 const configuration = new Configuration({
-  apiKey: "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",
+  apiKey: "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",
 });
 
 export const openai = new OpenAIApi(configuration);
